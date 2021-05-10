e24d13d22899a194230bd5d882700f2fd3a15398
Jim Rahman <jiminjamaica@gmail.com>
2021-05-10 19:32:52 -0400

    readme aded

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
-import * as React from "react";
+import React, { Component } from "react";
 import { Text, View, StyleSheet, SafeAreaView, StatusBar } from "react-native";
-import { Searchbar, Button } from "react-native-paper";
+import FlatListDemo from "./components/FlatList";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-          <Button mode="contained" onPress={() => console.log("worked")}>Search</Button>
-          <Text style={styles.paragraph}>
-            Search Your favorite github Repositories
-          </Text>
-        </View>
+      <SafeAreaView>
+        <FlatListDemo />
       </SafeAreaView>
     </>
   );
