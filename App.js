430f59a009d6c02d1dc4ec3263dfa50de36f353e
Jim Rahman <jiminjamaica@gmail.com>
2021-05-11 21:30:31 -0400

    WIP

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     backgroundColor: "#ecf0f1",
     marginTop: StatusBar.currentHeight,
   },
--- components/Search.js
+++ components/Search.js
@@ -1,46 +1,52 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, Text, View } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { FlatList, Text, View } from "react-native";
+import { Searchbar, Button, TextInput } from "react-native-paper";
 
 const Search = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [error, setError] = useState(null);
+  let list = [];
 
   const onChangeSearch = (query) => setSearchQuery(query);
+  const removeSpaceFromQuery = searchQuery.replace(" ", "");
 
-  const gitURL =
-    "https://api.github.com/search/repositories?q=" +
-    { searchQuery } +
-    "java&page=1&per_page=2";
+  const gitURL = `https://api.github.com/search/repositories?q=${removeSpaceFromQuery}&page=1&per_page=2`;
 
-    console.log(gitURL);
+  console.log(gitURL);
 
-  useEffect(() => {
-    fetch("")
+  const fetching = () => {
+    setLoading(true);
+    fetch(gitURL)
       .then((response) => response.json())
       .then((json) => {
         setData(json.items);
-        //console.log(json);
+        console.log(json);
       })
-      .catch((error) => {
-        console.error(error);
-        setError(error);
+      .catch((e) => {
+        console.error(e);
+        setError(e);
       })
       .finally(() => setLoading(false));
-  }, []);
+  };
   console.log(searchQuery);
 
   return (
-    <View>
-      <Searchbar
-        placeholder="Search"
-        onChangeText={onChangeSearch}
-        value={searchQuery}
-      />
-      <Text>{searchQuery}</Text>
-    </View>
+    error !== null && (
+      <View>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+        <TextInput mode="outlined" disabled={true} value={searchQuery} />
+        <Button mode="outlined" compact={true} onPress={() => fetching()}>
+          Search
+        </Button>
+        <Text>{searchQuery}</Text>
+      </View>
+    )
   );
 };
 
