1bb9173f256b12303d4bd8084f55a04eb30944b8
Jim Rahman <jiminjamaica@gmail.com>
2021-05-16 00:31:42 -0400

    tesing seach bar in app.js

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,5 +1,15 @@
-describe("Sanity description", ()=>{
-    it("Sanity test", ()=>{
-        expect(true).toBe(true)
-    })
-})
+import App from "../App";
+import { render } from "@testing-library/react-native";
+import React from "react";
+
+describe("Sanity description", () => {
+  it("Sanity test", () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe("App", () => {
+  it("renders serach bad", () => {
+    const { getByText } = render(<App />);
+  });
+});
