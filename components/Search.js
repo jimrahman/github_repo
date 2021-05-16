edd7e6ab27c4346d0a02bc0951f49faff995d415
Jim Rahman <jiminjamaica@gmail.com>
2021-05-16 07:58:11 -0400

    selected data successfully posted to docker server

--- components/Search.js
+++ components/Search.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, Text, View } from "react-native";
-import { Searchbar, Button, TextInput } from "react-native-paper";
+import {
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { Searchbar, Button } from "react-native-paper";
 
 const Search = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [error, setError] = useState(null);
+  const [selectedData, setSelectedData] = useState({});
 
   const onChangeSearch = (query) => setSearchQuery(query);
   const removeSpaceFromQuery = searchQuery.replace(" ", "");
@@ -27,8 +34,51 @@ const Search = () => {
       })
       .finally(() => setLoading(false));
   };
-  console.log(searchQuery);
-  console.log(data);
+
+  const postSelectedData = () => {
+    let postData = {
+      method: "POST",
+      body: JSON.stringify(selectedData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+    };
+    console.log(postData);
+    const dockerUrl = "http://192.168.0.107:8080/repo/";
+    fetch(dockerUrl, postData)
+      .then((response) => console.log(response.status))
+      .catch((e) => console.error(e));
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.list}>
+        <TouchableOpacity
+          onPress={() => {
+            setSelectedData({
+              id: item.id.toString(),
+              fullName: item.full_name,
+              createdAt: item.created_at,
+              stargazersCount: item.stargazers_count,
+              language: item.language,
+              url: item.url,
+            });
+            postSelectedData();
+          }}
+        >
+          <Text style={styles.wrapText}>ID: {item.id}</Text>
+          <Text style={styles.wrapText}>Name: {item.full_name}</Text>
+          <Text style={styles.wrapText}>Time Created: {item.created_at}</Text>
+          <Text style={styles.wrapText}>
+            StarGazer Count: {item.stargazers_count}
+          </Text>
+          <Text style={styles.wrapText}>Language: {item.language}</Text>
+          <Text style={styles.wrapText}>URL: {item.url}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   return (
     <>
@@ -51,11 +101,28 @@ const Search = () => {
         <FlatList
           data={data}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => <Text>{item.full_name}</Text>}
+          renderItem={renderItem}
         />
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  list: {
+    flex: 1,
+    padding: 8,
+  },
+  wrapText: {
+    backgroundColor: "#e9e9e9",
+    fontSize: 14,
+  },
+  opacityStyle: {
+    margin: 2,
+  },
+});
 
 export default Search;
