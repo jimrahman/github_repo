21ab855ffbbd61513294735860be41f54b32fba8
Rakibul Hasan <rakibulh9@gmail.com>
2021-05-17 15:09:27 -0400

    Test setup

--- components/tests/Search.test.js
+++ components/tests/Search.test.js
@@ -1,8 +1,13 @@
-describe("Seachbar", () => {
+import { fireEvent, render } from "@testing-library/react-native";
+import React from "react";
+import Search from "../Search";
+
+describe("Search", () => {
   describe("when the search button is clicked", () => {
     describe("and when github api returns list of repo successfully", () => {
       it("shows the list of th repo", () => {
-        expect(true).toBe(true);
+        const { getByTestId } = render(<Search />);
+        fireEvent.press(getByTestId("search-button"));
       });
     });
   });
